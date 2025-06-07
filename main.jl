85c8b7e546e81def3b0a48c5643ad643a108a158
kriszo74 <kriszo74@gmail.com>
2025-06-07 06:46:39 +0200

    3D inicializlás függvénybe ment

--- main.jl
+++ main.jl
@@ -28,36 +28,45 @@ Base.iterate(iter::ImpulzusIterátor, state=0) =
 origin = Idoforras(SVector(0.0, 0.0, 0.0), 0.5, 0.0)  # például RV = 0.5
 imp_iter = ImpulzusIterátor(origin, 0.05, 100)  # dt, lépésszám
 
+# --- 3D megjelenítő függvény ---
+function setup_scene(fig, use_axis3::Bool)
+    if use_axis3
+        return Axis3(fig[1, 1],
+            aspect = :data,
+            perspectiveness = 0.0,
+            elevation = π/2,
+            azimuth = 3π/2,
+            xgridvisible = false,
+            ygridvisible = false,
+            zgridvisible = false,
+            xspinesvisible = false,
+            xlabelvisible = false,
+            xticksvisible = false,
+            xticklabelsvisible = false,
+            yspinesvisible = false,
+            ylabelvisible = false,
+            yticksvisible = false,
+            yticklabelsvisible = false,
+            zspinesvisible = false,
+            zlabelvisible = false,
+            zticksvisible = false,
+            zticklabelsvisible = false
+        )
+    else
+        scene = LScene(fig[1, 1], show_axis = false)
+        cam3d!(scene, projectiontype = :orthographic, eyeposition = Vec3f(0,0,4), lookat = Vec3f(0, 0, 0), upvector = Vec3f(0, 1, 0))
+        return scene
+    end
+end
+
 # --- 3D megjelenítés ---
 fig = Figure(backgroundcolor = RGBf(0.302, 0.322, 0.471))  # #4D5278
-#scene = LScene(fig[1, 1], show_axis = false)
-# ax = Axis3(fig[1, 1], aspect = :data, perspectiveness = 0.0, elevation = π/2,  azimuth = 3π/2, xgridvisible = ...,
-scene = Axis3(fig[1, 1],
-    aspect = :data,
-    perspectiveness = 0.0,
-    elevation = π/2,
-    azimuth = 3π/2,
-    xgridvisible = false,
-    ygridvisible = false,
-    zgridvisible = false,
-    xspinesvisible = false,
-    xlabelvisible = false,
-    xticksvisible = false,
-    xticklabelsvisible = false,
-    yspinesvisible = false,
-    ylabelvisible = false,
-    yticksvisible = false,
-    yticklabelsvisible = false,
-    zspinesvisible = false,
-    zlabelvisible = false,
-    zticksvisible = false,
-    zticklabelsvisible = false
-)
-#cam3d!(scene, projectiontype = :orthographic, eyeposition = Vec3f(0,0,4), lookat = Vec3f(0, 0, 0),upvector = Vec3f(0, 1, 0))
+use_axis3 = true  # Itt lehet váltani
+scene = setup_scene(fig, use_axis3)
+
 
 # Impulzushatárok
 for g in imp_iter
-    #@infiltrate
     mesh!(scene, g, color = (RGBf(0.6, 1.0, 1.0), 0.05), transparency=true)  # türkiz (#99FFFF), 0.05 áttetszőség
 end
 
@@ -65,11 +74,10 @@ end
 dt = 0.05
 steps = 100
 pozíciók = [Point3f(origin.pozíció0[1] - origin.RV * dt * i, 0.0, 0.0) for i in 0:steps]
-#@infiltrate
 lines!(scene, pozíciók, color=:white, linewidth=1.5)
 
 # --- Kamera automatikus illesztése vagy kézi limits beállítás ---
-# autolimits!(ax)  # automatikusan belenagyít az összes objektum alapján
-# limits!(ax, FRect3D(Point3f(-1, -1, -1), Vec3f(2, 2, 2)))  # kézi bounding box, ha fix területet akarunk
+# autolimits!(scene)
+# limits!(scene, FRect3D(Point3f(-1, -1, -1), Vec3f(2, 2, 2)))
 
 fig
