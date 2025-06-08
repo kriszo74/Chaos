75884ace97120bb8597292c82e58777c4c3d32b0
kriszo74 <kriszo74@gmail.com>
2025-06-08 07:04:28 +0200

    Move setup_scene to separate file

--- main.jl
+++ main.jl
@@ -28,36 +28,8 @@ Base.iterate(iter::ImpulzusIterátor, state=0) =
 origin = Idoforras(SVector(0.0, 0.0, 0.0), 0.5, 0.0)  # például RV = 0.5
 imp_iter = ImpulzusIterátor(origin, 0.05, 100)  # dt, lépésszám
 
-# --- 3D megjelenítő függvény ---
-function setup_scene(fig, use_axis3::Bool)
-    if use_axis3
-        return Axis3(fig[1, 1],
-            aspect = :data,
-            perspectiveness = 0.0,
-            elevation = π/2,
-            azimuth = 3π/2,
-            xgridvisible = false,
-            ygridvisible = false,
-            zgridvisible = false,
-            xspinesvisible = false,
-            xlabelvisible = false,
-            xticksvisible = false,
-            xticklabelsvisible = false,
-            yspinesvisible = false,
-            ylabelvisible = false,
-            yticksvisible = false,
-            yticklabelsvisible = false,
-            zspinesvisible = false,
-            zlabelvisible = false,
-            zticksvisible = false,
-            zticklabelsvisible = false
-        )
-    else
-        scene = LScene(fig[1, 1], show_axis = false)
-        cam3d!(scene, projectiontype = :orthographic, eyeposition = Vec3f(0,0,4), lookat = Vec3f(0, 0, 0), upvector = Vec3f(0, 1, 0))
-        return scene
-    end
-end
+include("scene_setup.jl")
+
 
 # --- 3D megjelenítés ---
 fig = Figure(backgroundcolor = RGBf(0.302, 0.322, 0.471))  # #4D5278
